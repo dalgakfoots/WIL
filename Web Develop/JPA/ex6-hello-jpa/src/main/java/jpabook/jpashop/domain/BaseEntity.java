95c86217cb603f81c5a1b863a0bfb0d5b984de71
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2021-11-25 14:02:40 +0900

    Feat : "JPA 엔티티 - @Embeddable @Embedded 실습" 값타입 실습

---
+++ Web Develop/JPA/ex6-hello-jpa/src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    private String createdBy;
+    private LocalDateTime createdDateTime;
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedTime;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    public void setLastModifiedTime(LocalDateTime lastModifiedTime) {
+        this.lastModifiedTime = lastModifiedTime;
+    }
+}
+
