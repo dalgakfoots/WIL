2bff48782077e1df4e603a8bea021f09f9f54769
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2021-12-08 10:03:18 +0900

    Feat : "주문조회 API V2" 1. 주문 엔티티를 DTO로 변환하여 반환하도록 처리 2. N+1 문제 발생 가능성 있음.

--- Web Develop/Spring/Practice! Using Spring Boot and JPA 2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ Web Develop/Spring/Practice! Using Spring Boot and JPA 2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  *
@@ -32,5 +37,36 @@ public class OrderSimpleApiController {
             order.getDelivery().getId(); // LAZY loading 되어있는 엔티티 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // 엔티티를 DTO로 변환하여 리턴한다.
+
+        // N + 1  문제 발생 가능성 있음.
+        // 주문 1 + 회원 (N) + 배송 (N)
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
