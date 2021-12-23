7c3940d5004682392cf6262842753d5e464edcef
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2021-12-23 10:10:12 +0900

    Feat : "QueryDSL 기능 간단히 사용해보기" 1. QueryDSL 사용하기 위한 Gradle 설정 2. 간단한 조회 쿼리 작성

--- Web Develop/Spring/Practice! Using Spring Boot and JPA 2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Web Develop/Spring/Practice! Using Spring Boot and JPA 2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,7 +1,12 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.api.OrderSimpleApiController;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -94,6 +99,35 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    // QueryDSL 로 쿼리 작성
+    public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
+    }
+
     public List<Order> findAllWithMemberDelivery() {
         // Fetch 조인 적용
         return em.createQuery("select o from Order o" +
@@ -103,8 +137,8 @@ public class OrderRepository {
 
     public List<Order> findAllWithMemberDelivery(int offset, int limit) {
         return em.createQuery("select o from Order o" +
-                " join fetch o.member m " +
-                " join fetch o.delivery d", Order.class)
+                        " join fetch o.member m " +
+                        " join fetch o.delivery d", Order.class)
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
@@ -117,11 +151,11 @@ public class OrderRepository {
         // 페이징 불가
         // 1개 이상의 컬렉션 조인은 금지 <- 1:n:m
         return em.createQuery(
-                "select distinct o from Order o"+
-                " join fetch o.member m" +
-                        " join fetch o.delivery d" +
-                        " join fetch o.orderItems oi" +
-                        " join fetch oi.item i",Order.class)
+                        "select distinct o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d" +
+                                " join fetch o.orderItems oi" +
+                                " join fetch oi.item i", Order.class)
                 .getResultList();
     }
 }
