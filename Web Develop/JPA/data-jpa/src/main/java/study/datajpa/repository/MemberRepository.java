c6a94f8e40716d57825e5a112e75e26fe56c9534
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2021-12-24 12:31:30 +0900

    Feat : "Repository 메소드에 쿼리를 직접 정의" 1. 이름이 없는 NamedQuery 가 생성된다.

--- Web Develop/JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ Web Develop/JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member , Long> { // 엔티티 , 엔티티 Id 타입
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age); // 필드명 변경 시, 메서드 이름도 변경해야 함.
+
+    @Query("select m from Member m where m.username = :username and m.age =:age") // 어플리케이션 로딩 시점에 파싱함 -> 문법 오류 시 에러
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
