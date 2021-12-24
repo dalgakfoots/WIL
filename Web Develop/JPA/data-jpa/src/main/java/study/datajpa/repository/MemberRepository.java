eac4529f80c550493e25f4df5bf91ea9c6fe65ce
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2021-12-24 12:11:10 +0900

    Feat : "메소드 이름으로 쿼리 생성" 1. JpaRepository 를 상속받은 인터페이스는 정해진 규약의 이름을 가진 메소드를 생성함으로 쿼리를 간단히 생성할 수 있다.

--- Web Develop/JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ Web Develop/JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member , Long> { // 엔티티 , 엔티티 Id 타입
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age); // 필드명 변경 시, 메서드 이름도 변경해야 함.
 }
