0303e1401edf4673a250125cb72bebc4eb35ae17
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2021-12-27 14:13:29 +0900

    Feat : "Web 확장 및 Spring Data JPA 구현체 테스트"

---
+++ Web Develop/JPA/data-jpa/src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,42 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+
+    // Spring Data JPA 의 save 는 (id == null ?)분기에 따라 persist 또는 merge 됨.
+    // 식별자 생성 시 @GeneratedValue 를 사용하지 않을 경우, Persistable 인터페이스를 구현함으로서 merge 되는 것을 해결
+
+    @Id
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+}
