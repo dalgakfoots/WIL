f66cb7d6251b781da1256d3645993e2d74e770a4
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2021-12-27 10:11:44 +0900

    Feat : "쿼리 힌트 및 락"

--- Web Develop/JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ Web Develop/JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,14 +2,13 @@ package study.datajpa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.List;
 import java.util.Optional;
 
@@ -62,4 +61,13 @@ public interface MemberRepository extends JpaRepository<Member , Long> { // 엔�
 
     @EntityGraph(attributePaths = {"team"})
     List<Member> findEntityGraphByUsername(@Param("username") String username);
+
+    // 쿼리 힌트
+    @QueryHints(@QueryHint(name = "org.hibernate.readOnly" , value= "true"))
+    Member findReadOnlyByUsername(String username);
+
+    // select for update
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUsername(String username);
+
 }
